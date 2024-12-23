c920627cc9b8af8affbbf437ffce201bdaf59da9
ziyech18 <jassimalikk8@gmail.com>
2024-12-23 19:32:02 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "jnet"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "jasim"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "124566"
 )
 
 var ID ids.ID
